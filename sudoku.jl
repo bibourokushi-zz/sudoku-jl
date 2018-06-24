1aa670a2b6e2c3a17ce4515c32cf7909933458b3
nobuyuki hikichi <bibourokushi@gmail.com>
2018-06-24 20:08:04 +0900

    Bug fix for syntax error, and run well.

--- sudoku.jl
+++ sudoku.jl
@@ -2,11 +2,58 @@
 # sudoku.jl: simple sudoku solver ported from python.
 #
 
+using Printf
+
+struct CandXY
+    gridX::Int8
+    gridY::Int8
+    cand::Array{Bool}
+end
+
+
+
 function setup_grid(id)
     #println(id)
     grid = ""
-    if id == 1
-        grid = "000000000"
+    if id == -1 # For dev
+        grid  = "287465193"
+        grid *= "691238745"
+        grid *= "435719826"
+        grid *= "369124587"
+        grid *= "814357962"
+        grid *= "572986314"
+        grid *= "000000000"
+#        grid *= "726591438"
+        grid *= "000000000"
+#        grid *= "958643271"
+        grid *= "000000000"
+#        grid *= "143872659"
+    end
+    if id == 0 # full fill check
+        grid  = "287465193"
+        grid *= "691238745"
+        grid *= "435719826"
+        grid *= "369124587"
+        grid *= "814357962"
+        grid *= "572986314"
+        grid *= "726591438"
+        grid *= "958643271"
+        grid *= "143872659"
+    end
+    if id == 1 # Very easy
+        grid  = "000349806"
+        grid *= "368072409"
+        grid *= "407000253"
+
+        grid *= "050107904"
+        grid *= "780096035"
+        grid *= "030405062"
+        grid *= "072604301"
+        grid *= "600703528"
+        grid *= "503928000"
+    end
+    if id == 2
+        grid  = "000000000"
         grid *= "000000027"
         grid *= "400608000"
         grid *= "071000300"
@@ -17,16 +64,6 @@ function setup_grid(id)
         grid *= "046819205"
     end
     return grid
-end
-
-function count_bit(b)
-    c = 0
-    for i = 1:9
-        if b[i]
-            c += 1
-        end
-    end
-    return c
 end
 
 
@@ -51,9 +88,10 @@ function gen_bit_array(grid, debug=false)
 end
 
 function toStringList(b)
+    # b: bit array according 1..9, num
     res = ""
     for i = 1 : 9
-        #print(b[i])
+        #print(" i/@toStringList=", i, ",b=",b[i])
         if b[i]
             res *= string(i)
         end
@@ -77,13 +115,70 @@ function tostring(b)
     return "0"
 end
 
+function or_line_disp(por_line)
+    for j = 1 : 9
+        println("or line: [", j, "], index=", j)
+        println(" ", por_line[:,j])
+        if (j%3) == 0
+            if j != 9
+                print(" ")
+            end
+        end
+    end
+end
 
-function display_grid(grid)
+function is_full(pgrid, debug=false)
+    or_line = falses(9, 9)
     for i = 0 : 8
         for j = 1 : 9
             index = i*9+j
-            #println(i, ",", j, "=", index)
-            print(tostring(bgrid[:,index]))
+            if debug
+                println("[", i, ",", j, "], index=", index)
+                println(typeof(pgrid))
+                println(pgrid[:,index])
+            end
+            if debug
+                if (j%3) == 0
+                    if j != 9
+                        print(" ")
+                    end
+                end
+            end
+            if debug
+                println(typeof(pgrid))
+            end
+            for k = 1:9
+                if debug
+                    println(" [", k, "]=", typeof(pgrid[k, index]))
+                    println(" or[", k, "]=", typeof(or_line[k, j]))
+                end
+                #or_line[k, j] = or_line[k,j] | pgrid[k, index]
+            end
+            or_line[:,j] .|= pgrid[:,index]
+        end
+        if debug
+            or_line_disp(or_line)
+            println("")
+            if (i%3)==2
+                println("")
+            end
+        end
+    end
+    if debug
+        or_line_disp(or_line)
+        print(" +/all=", all(x->x==true, or_line))
+    end
+    return all(x->x==true, or_line)
+end
+
+function display_grid(pgrid, debug=false)
+    for i = 0 : 8
+        for j = 1 : 9
+            index = i*9+j
+            if debug
+                println("\n[", i, ",", j, "]@", index,":", pgrid[:,index])
+            end
+            print(tostring(pgrid[:,index]))
             if (j%3) == 0
                 if j != 9
                     print(" ")
@@ -106,11 +201,19 @@ function candidate(grid, ppoint, debug=false)
     end
     
     # Line
-    #println("  ", y*9+1, " to ", y*9+10-1)
+    if debug
+        println("  ", y*9+1, " to ", y*9+10-1)
+    end
     lc = falses(9)
-    #println(lc)
     for i = y*9+1:y*9+10-1
         lc = lc.|grid[:, i]
+        if debug
+            @printf(" lc/.(%d):", i)
+            println(grid[:, i])
+        end
+    end
+    if debug
+        println(" lc/+:", lc)
     end
     lc = full_clist.&.~lc
     if debug
@@ -174,15 +277,17 @@ function candidate(grid, ppoint, debug=false)
 end
 
 
-full      = trues(9, 9*9)
+
 # "0" means all zero(falses)
 zero_grid = falses(9)
 
-function solver(grid, debug=false)
-    if grid == full
-        if debug
-            println("full fill@solver")
-        end
+function solver(grid, debug=false, level=0)
+    #res = is_full(grid, debug)
+    res = is_full(grid)
+    if debug
+        println("   is_full ret:", res)
+    end
+    if res 
         return true, grid
     end
     
@@ -202,31 +307,87 @@ function solver(grid, debug=false)
                 end
                 continue
             end
-            clist = candidate(grid, (ix, iy), false)
-            clen  = count_bit(clist)
+            clist = candidate(grid, (ix, iy), debug)
+            #clen  = count_bit(clist)
+            clen  = length(clist[clist .== true])
+	    # Canidite list: [# cand] = [...]
             if get(grid_clist, "$clen:", "") == ""
                 if debug
-                    @printf(" len(%d)/-=0\n", clen)
+                    @printf("<%d> len(%d)/-=0\n", level, clen)
                 end
-                grid_clist["$clen:"] = Vector(clist)
+                #grid_clist["$clen:"] = [[ix, iy], [clist]]
+                grid_clist["$clen:"] = CandXY[]
+                push!(grid_clist["$clen:"], CandXY(ix, iy, clist))
             else
                 if debug
-                    @printf(" len(%d)/-=%d\n",
-                            clen, length(grid_clist["$clen:"]))
+                    @printf("<%d> len(%d)/-=%d\n",
+                            level, clen, length(grid_clist["$clen:"]))
                     println(typeof(grid_clist["$clen:"]))
                 end
-                append!(grid_clist["$clen:"], clist)
+                cclist = grid_clist["$clen:"]
+                #append!(grid_clist["$clen:"], [[ix, iy], clist])
+                #append!(grid_clist["$clen:"], CandXY(ix, iy, clist))
+                push!(grid_clist["$clen:"], CandXY(ix, iy, clist))
             end
             if debug
-                @printf("clist(%d,%d):<%d>%s\n", ix, iy, clen,
+                @printf("<%d>clist(%d,%d):[%d]%s\n", level, ix, iy, clen,
                         toStringList(clist))
+                println(typeof(grid_clist["$clen:"]))
                 println(" len/+=", length(grid_clist["$clen:"]))
             end
         end
     end
 
-    for i = 0:9
-        println(i,":", length(grid_clist[i+1]))
+    for i = 1:9
+	try
+	    if get(grid_clist,"$i:","") == ""
+	    	continue
+            end
+        catch KeyError
+                continue
+	end
+        if debug
+            println(typeof(grid_clist["$i:"]))
+    	    println(i,":", length(grid_clist["$i:"]))
+        end
+        cclist = grid_clist["$i:"]
+        for cc in cclist
+            #println(" cc:", cc)
+            #println(typeof(cc))
+            #[[ix, iy], clist] = cc
+            #ix = cc[1][1]
+            #iy = cc[1][2]
+            #clist = cc[2]
+            if debug
+                @printf("<%d> [%d, %d]: %s\n",
+                        level, cc.gridX, cc.gridY, toStringList(cc.cand))
+            end
+            # Put cc.and into [gridX, gridY]
+            for i = 1: 9
+                if cc.cand[i]
+                    # Put
+                    grid[i, cc.gridY*9+cc.gridX] = true
+                    res, grid = solver(grid, debug, level+1)
+                    if res == true
+                        return res, grid
+                    end
+                    # Unput
+                    grid[i, cc.gridY*9+cc.gridX] = false
+                end
+            end
+
+            if false
+                # Put cc.and into [gridX, gridY]
+                grid[cc.cand, cc.gridY*9+cc.gridX] .= true
+                res, grid = solver(grid)
+                if res == true
+                    return res, grid
+                end
+                # Unput
+                grid[cc.cand, cc.gridY*9+cc.gridX] .= false
+            end
+
+        end
     end
 
     return false, grid
@@ -234,27 +395,35 @@ end
 
 
 if !isinteractive()
+    println("Start.")
     debug = false
+    #debug = true
     prog = basename(Base.source_path())
     argc = size(ARGS,1)
-    #println(argc)
+    if debug
+        println("argc=", argc)
+    end
     if argc != 1
         println("Usage: julia ", prog, " ID")
-        #println(basename(@__FILE__))
+        if debug
+            println(basename(@__FILE__))
+        end
     else
         a1 = ARGS[1]
         id = parse(Int32, a1)
         grid = setup_grid(id)
         println(" Input:")
         bgrid = gen_bit_array(grid)
+        #display_grid(bgrid, debug)
         display_grid(bgrid)
     end
-    #status, rgrid = solver(bgrid,  false)
+    status, rgrid = solver(bgrid,  debug)
     if debug
-        println("given full fill")
-        status, rgrid = solver(full,  true)
+        println("  status=", status)
     end
-    status, rgrid = solver(bgrid,  true)
-    println(" =>Result:")
-    println("  status=", status)
+    if status
+        println(" =>Result:")
+        display_grid(bgrid)
+    end
+    println("Finished.")
 end
